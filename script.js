98e8aaf536c2f6f45bc8087010d0bc4be4634ddb
Asifcoder-art <111686415+Asifcoder-art@users.noreply.github.com>
2023-03-05 14:08:59 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const ChaneText = document.getElementById("ChaneText");
+const button =document.getElementById("button");
+
+button.addEventListener("dblclick",()=>{
+    ChaneText.innerHTML = "Welcome To Programming";
+})
